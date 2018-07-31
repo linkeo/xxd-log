c2efaf7d41788bc9dc710108913e8c1d38aa934b
林康 <linkang@innobuddy.com>
2018-07-31 10:19:25 +0800

    feat: add d.ts

---
+++ index.d.ts
@@ -0,0 +1,30 @@
+import { Chalk } from 'chalk';
+
+interface LoggerOptions {
+  withTimestamp?: boolean;
+  withLabel?: boolean;
+  timestampFormat?: string;
+  highlightStackTrace?: boolean;
+}
+
+declare class Logger {
+  constructor(options: LoggerOptions);
+  withTimestamp: boolean;
+  withLabel: boolean;
+  timestampFormat: string;
+  chalk: Chalk;
+  Logger: Logger;
+  log(message?: any, ...optionalParams: any[]): void;
+  echo(message?: any, ...optionalParams: any[]): void;
+  println(message?: any, ...optionalParams: any[]): void;
+  trace(message?: any, ...optionalParams: any[]): void;
+  debug(message?: any, ...optionalParams: any[]): void;
+  info(message?: any, ...optionalParams: any[]): void;
+  warn(message?: any, ...optionalParams: any[]): void;
+  error(message?: any, ...optionalParams: any[]): void;
+  fatal(message?: any, ...optionalParams: any[]): void;
+}
+
+declare const defaultInstance: Logger;
+
+export = defaultInstance;
